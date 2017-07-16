03ab3b7557646c2f8376ac2e53e57d785c3d2159
Yosuke Ushigome <ushi@takram.com>
2017-07-16 22:08:13 +0100

    refactored wrapSingleByteTexts

--- src/templates/about.js
+++ src/templates/about.js
@@ -1,58 +1,22 @@
 import React from "react"
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import wrapSingleByteTexts from '../utils/wrapSingleByteTexts'
 
 class AboutTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
-    var wrapTag = '<span class="hankaku">';
-  	var wrapTag_end = '</span>';
-  	var isTag = false;
-  	var isHan = false;
+    const html = wrapSingleByteTexts(post.html, 'hankaku')
 
-  	var re = new RegExp(/[\da-z\ \.\-\!"#\$%&'\(\)=\^~\|@`\[\{\]\}\*\:\+;\?\/\,_\\]/i);
-  	var html = post.html;
-  	html = html.replace(/\s+>/g, '>');
-
-  	var i;
-  	for(i=0; i<html.length; i++){
-  		if (isTag===false){
-  			if (html[i]=='<'){
-
-  				isTag = true;
-
-  				if (isHan===true){
-  					html = html.substr(0,i)+wrapTag_end+html.substr(i);
-  					i += wrapTag_end.length;
-  					isHan = false;
-  					continue;
-  				}
-
-  			}
-  		} else {
-  			if (html[i]=='>'){
-  				isTag = false;
-  			}
-  		}
-
-  		if (isTag===false){
-  			if (html[i].match(re)){
-  				if (isHan===false){
-  					html = html.substr(0,i)+wrapTag+html.substr(i);
-  					i += wrapTag.length;
-  				}
-  				isHan = true;
-  			} else {
-  				if (isHan===true){
-  					html = html.substr(0,i)+wrapTag_end+html.substr(i);
-  					i += wrapTag_end.length;
-  				}
-  				isHan = false;
-  			}
-  		}
-
-  	}
     return (
-      <div className="content" dangerouslySetInnerHTML={{ __html: html }} />
+      <div>
+        <Grid fluid>
+          <Row around="sm">
+            <Col sm={12} md={8}>
+              <div className="content" dangerouslySetInnerHTML={{ __html: html }} />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     )
   }
 }
