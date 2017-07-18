b8b297e0133d0311173673a9cee87fb4ab76c925
Yosuke Ushigome <ushi@takram.com>
2017-07-18 09:27:39 +0100

    added react-headroom to the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from "prop-types"
-
 import Link from "gatsby-link"
+
+import Headroom from "react-headroom"
 
 import MainColumn from './mainColumn'
 
@@ -13,27 +14,28 @@ class Header extends React.Component {
   render() {
     const { pathname } = this.props.location
     const isAbout = pathname === "/about"
-
     return (
-      <header className="header">
-        <MainColumn>
-          <div>
-            <h1 className="title is-1">I’m Yosuke Ushigome, a creative technologist based in London.</h1>
-          </div>
-          <div className="level">
-            <div className="level-left">
-              <div className="level-item">
-                <h2 className="title is-2">I demystify emerging technology through prototyping.</h2>
+      <Headroom>
+        <div className="header">
+          <MainColumn>
+            <div>
+              <h1 className="title is-1">I’m Yosuke Ushigome, a creative technologist based in London.</h1>
+            </div>
+            <div className="level">
+              <div className="level-left">
+                <div className="level-item">
+                  <h2 className="title is-2">I demystify emerging technology through prototyping.</h2>
+                </div>
+              </div>
+              <div className="level-right">
+                <div className="level-item has-text-right"  style={{marginLeft: '2rem'}}>
+                  <h2 className="title is-2"><Link to={isAbout ? "/" : "/about"}>{isAbout ? "Less" : "More"} about me</Link></h2>
+                </div>
               </div>
             </div>
-            <div className="level-right">
-              <div className="level-item has-text-right"  style={{marginLeft: '2rem'}}>
-                <h2 className="title is-2"><Link to={isAbout ? "/" : "/about"}>{isAbout ? "Less" : "More"} about me</Link></h2>
-              </div>
-            </div>
-          </div>
-        </MainColumn>
-      </header>
+          </MainColumn>
+        </div>
+      </Headroom>
     )
   }
 }
