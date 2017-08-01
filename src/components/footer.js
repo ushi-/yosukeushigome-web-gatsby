f16729752c45314f3f05ab6d2fab5f86d041de5c
Yosuke Ushigome <ushi@takram.com>
2017-08-02 00:43:55 +0100

    centered the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,12 +9,14 @@ class Footer extends React.Component {
     return (
       <footer className="footer">
         <MainColumn>
-          <p>
-            <span className="icon is-small"><i className="fa fa-copyright" aria-hidden="true"></i></span> {year} Yosuke Ushigome
-          </p>
-          <p>
-            The source code of this website is licensed <a href="http://opensource.org/licenses/mit-license.php" target="blank">MIT</a> and is accessible on <a href="https://github.com/ushi-/yosukeushigome-web-gatsby" target="blank">Github</a>.
-          </p>
+          <div className="has-text-centered">
+            <p>
+              <span className="icon is-small"><i className="fa fa-copyright" aria-hidden="true"></i></span> {year} Yosuke Ushigome
+            </p>
+            <p>
+              The source code of this website is licensed <a href="http://opensource.org/licenses/mit-license.php" target="blank">MIT</a> and is accessible on <a href="https://github.com/ushi-/yosukeushigome-web-gatsby" target="blank">Github</a>.
+            </p>
+          </div>
         </MainColumn>
       </footer>
     )
